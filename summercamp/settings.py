92c31efc0065e51397369c71c267958ce47c8bdd
zvukmiro <zvukmiro@gmail.com>
2019-10-05 12:23:00 -0700

    settings django debug wording

--- summercamp/settings.py
+++ summercamp/settings.py
@@ -26,7 +26,7 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', default=False, cast=bool)
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', default=False))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gec-summercamp.herokuapp.com']
 
