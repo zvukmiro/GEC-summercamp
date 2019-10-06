54d5d596d2d397c840aaed864c86c3a5e507afa5
zvukmiro <zvukmiro@gmail.com>
2019-10-05 20:55:05 -0700

    databases with postgres using configvars

--- summercamp/settings.py
+++ summercamp/settings.py
@@ -79,16 +79,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'summercamp.wsgi.application'
 
-from decouple import config
 
 # Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '',
         }
 }
